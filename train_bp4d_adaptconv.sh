bd38c996362ca46903f9e7e8c49cb0d88d173500
hanshizhong1105@gmail.com <hanshizhong1105@gmail.com>
2017-02-10 17:04:19 -0500

    roll back to the nearest neighbor cutting and padding version for three layer adaptive together

--- train_bp4d_adaptconv.sh
+++ train_bp4d_adaptconv.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-foldname=GSnormWarpImageClear
-aulist=(2 12 17 25 28 45)
+foldname=normImage
+aulist=(1 2 4 6 7 10 12 14 15 17 23)
 run=$2
-#au=$3
-dir=/home/handong/Shizhong/FERA2015/data_steps/
+dir=/home/handong/Shizhong/BP4D/data_steps/
 datadir=$dir/DRMF_CUDA/
 #for run in 1 2 3 4 5 #6 7 8 9 10
 #do
-    resdir=$dir/testResult/${foldname}_adaptconv3nn_run${run}/
+    resdir=$dir/testResult/${foldname}_adaptconv2_run${run}/
     mkdir $resdir
-    for iau in 0 1 2 3 4 5 #${au}
+    for iau in 0 1 2 3 4 5 6 7 8 9 10 
     do
     curAU=${aulist[${iau}]}
     scorefile=$resdir/outscore_baseline${curAU}.txt
@@ -18,7 +17,7 @@ datadir=$dir/DRMF_CUDA/
     if [ ! -e "$scorefile" ]
     then
     	echo "$scorefile not exist"
-    	./models/fera2015/solver_train_adaptconv.sh ${curAU} ${datadir} ${resdir} $foldname
+    	./models/bp4d/solver_train_adaptconv.sh ${curAU} ${datadir} ${resdir} $foldname
     	./build/tools/caffe train -gpu $1 -solver=$resdir/solver${curAU}.prototxt  #-weights=/media/datadisk/database/FERA2015/data_steps/CNNData/normWarpImage/AU_binary_fromFera2015.caffemodel
     fi
     done
