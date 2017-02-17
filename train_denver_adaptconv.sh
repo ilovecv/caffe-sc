4ce9cce17cdcbea535e0b85769c42b5d431c4458
hanshizhong1105@gmail.com <hanshizhong1105@gmail.com>
2017-02-17 11:16:10 -0500

    take the 1 layer adaptive conv as separate shell script file

--- train_denver_adaptconv.sh
+++ train_denver_adaptconv.sh
@@ -10,7 +10,7 @@ datadir=$dir/DRMF_CUDA/
 #do /
     for run in 1 2 3 4 5
     do
-	resdir=$dir/testResult/${foldname}_adaboostst_set${set}_run${run}/
+	resdir=$dir/testResult/${foldname}_adaptive_set${set}_run${run}/
 	mkdir $resdir
         for i in  0 1 2 3 4 5 6 7 8 9 10 11
         do
@@ -20,7 +20,7 @@ datadir=$dir/DRMF_CUDA/
         if [ ! -e "$scorefile" ]
         then
     	    echo "$scorefile not exist"
-            ./models/denver/solver_train_adaptiveconv.sh ${curAU} ${set} ${datadir} ${resdir}
+            ./models/denver/solver_train_adaptconv.sh ${curAU} ${set} ${datadir} ${resdir} ${foldname}
             ./build/tools/caffe train -gpu $1 -solver=$resdir/solver${curAU}.prototxt  #-weights=/media/datadisk/database/FERA2015/data_steps/CNNData/normWarpImage/AU_binary_fromFera2015.caffemodel
         fi
         done
